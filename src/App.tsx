906886bfc37809d9069c68a43c680d57ea3645d2
Xiangxu Lin <cricel.design@gmail.com>
2024-05-25 14:41:13 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Iframe from 'react-iframe'
+
+import CountdownTimer from './CountdownTimer';
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Iframe className='left_panel' url="https://calendar.google.com/calendar/embed?src=cricel.design%40gmail.com&ctz=America%2FNew_York"/>
+      <div className='right_panel'>
+        <CountdownTimer targetDate="2024-09-15T23:59:59" headerText="ICRA" />
+        <CountdownTimer targetDate="2024-10-01T23:59:59" headerText="HRI" />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/CountdownTimer.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+
+interface CountdownTimerProps {
+  targetDate: string;
+  headerText: string;
+}
+
+const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate, headerText }) => {
+  const colors = ['#7AB8BF', '#C4EEF2', '#9FC131', '#DBF227', '#D6D58E', '#FFCB9A', '#F2E7DC', '#EDAA25'];
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+    let timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [bgColor, setBgColor] = useState(getRandomColor());
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  }, [timeLeft]);
+
+  const styles = {
+    container: {
+      textAlign: 'center' as 'center',
+      backgroundColor: bgColor,
+      padding: '1px',
+      borderRadius: '10px',
+      margin: '10px',
+    },
+  };
+
+  return (
+    <div style={styles.container}>
+      <h2>{headerText}</h2>
+      <h1>{`${timeLeft.days}d ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}</h1>
+    </div>
+  );
+};
+
+export default CountdownTimer;
