32d7e5b15bf5d1b6e17599daaf051f0346454bf1
Xiangxu Lin <cricel.design@gmail.com>
2024-05-25 20:05:23 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <>
       <Iframe className='left_panel' url="https://calendar.google.com/calendar/embed?src=cricel.design%40gmail.com&ctz=America%2FNew_York"/>
       <div className='right_panel'>
-        <CountdownTimer targetDate="2024-09-15T23:59:59" headerText="ICRA" />
-        <CountdownTimer targetDate="2024-10-01T23:59:59" headerText="HRI" />
+        <CountdownTimer targetDate="2024-09-15T23:59:59" headerText="ICRA Conference" />
+        <CountdownTimer targetDate="2024-10-01T23:59:59" headerText="HRI Conference" />
       </div>
     </>
   )
