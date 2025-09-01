383deba474f7c9ce5d0c19b780967a4fed6b96e5
Xiangxu Lin <cricel.design@gmail.com>
2025-08-31 23:22:49 -0400

    feat: add test conference

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <Iframe className='left_panel' url="https://calendar.google.com/calendar/embed?src=atrlab.kent.public%40gmail.com&ctz=America%2FNew_York"/>
       <div className='right_panel'>
-        <CountdownTimer targetDate="2024-09-15T23:59:59" headerText="ICRA Conference" />
+        <CountdownTimer targetDate="2025-10-15T23:59:59" headerText="Test Conference" />
         <CountdownTimer targetDate="2024-09-20T23:59:59" headerText="CICT Conference" />
         <CountdownTimer targetDate="2024-10-01T23:59:59" headerText="HRI Conference" />
       </div>
